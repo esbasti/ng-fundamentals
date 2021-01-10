01e78f53a5f15b520d547e31f2eb48a6aae73c7d
Sebastian <esbasti@gmail.com>
2021-01-10 01:13:28 -0600

    Creating Event Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/nav-bar.component'
+import { EventService } from './shared/events.service'
 
 @NgModule({
   imports: [
     BrowserModule
+  ],
+  providers: [
+    EventService
   ],
   declarations: [
     EventsAppComponent,
