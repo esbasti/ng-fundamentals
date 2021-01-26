02f9d29239f509858853cff8f0a43822d0e8f576
Sebastian Villalobos <esbasti@gmail.com>
2021-01-26 17:06:35 -0600

    Implementing Validating Reactive Forms

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,33 +1,56 @@
+import { style } from '@angular/animations'
 import { Component, OnInit } from '@angular/core'
-import {FormControl, FormGroup} from '@angular/forms'
+import {FormControl, FormGroup, Validators} from '@angular/forms'
 import { Router } from '@angular/router'
 import { AuthService } from './auth.service'
 
 @Component({
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styles: [`
+  em{ float:right; color:#E05C65; padding-left: 10px;}
+  .error input {background-color:#E3C3CE;}
+  .error ::-webkit-input-placeholder { color: #999; }
+  .error ::-moz-placeholder { color: #999; }
+  .error :-moz-placeholder { color: #999; }
+  .error ::ms-input-placeholder { color: #999; }
+  `]
 })
 
 export class ProfileComponent implements OnInit {
-  
+
   profileForm: FormGroup
+  private firstName: FormControl
+  private lastName: FormControl
 
   constructor(private auth:AuthService, private router:Router){
   }
 
   ngOnInit() {
-    let firstName = new FormControl(this.auth.currentUser.firstName)
-    let lasttName = new FormControl(this.auth.currentUser.lastName)
+    this.firstName = new FormControl(this.auth.currentUser.firstName, Validators.required)
+    this.lastName = new FormControl(this.auth.currentUser.lastName, Validators.required)
 
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lasttName
+      firstName: this.firstName,
+      lastName: this.lastName
     })
   }
 
   saveProfile(formValues) {
-    this.auth.updateUser(formValues.firstName, formValues.lastName)
-    this.router.navigate(["events/"])
+    if (this.profileForm.valid) {
+      this.auth.updateUser(formValues.firstName, formValues.lastName)
+      this.router.navigate(["events/"])
+    }
   }
+
+  validateFirstName() {
+    console.log(this.firstName.valid);
+    console.log(this.firstName.untouched);
+     return this.firstName.valid || this.firstName.untouched
+  }
+
+  validateLastName() {
+    return this.lastName.valid || this.lastName.untouched
+ }
 
   cancel() {
     this.router.navigate(["events/"])
